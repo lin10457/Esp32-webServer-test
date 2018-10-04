d4d0f9aa50d4d08ea5631260f78d8f6f9bbc5e0e
whisper07 <lin10457@gmail.com>
2018-10-04 14:48:49 +0800

    first

---
+++ HelloServer.ino
@@ -0,0 +1,92 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+const char* ssid = "icheerus";
+const char* password = "a0963170671";
+
+WebServer server(80);
+
+const int led = 2;
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "喜羊羊與灰太郎");
+  digitalWrite(led, 0);
+}
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+
+void handleT0(){
+  //server.send(200,"text/plain",String("T0:")+touchRead(T0));
+  server.sendHeader("Access-Control-Allow-Origin","*");
+  server.send(200,"text/json",String("{\"A0\":")+touchRead(T0)+"}");
+} 
+
+void a(){
+  server.send(200,"text/plain","this is Blink Function!");
+  for(int i = 0; i < 15 ;i++){
+    digitalWrite(led,1);
+    delay(500);
+    digitalWrite(led,0);
+    delay(500);
+  }
+}
+
+
+void setup(void) {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+  server.on("/Blink", a);
+  server.on("/T0", handleT0);
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+}
